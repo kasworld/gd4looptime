5c3a2517703f3053936a216247a675fca1cfe66e
seukwonkang <kasworld@gmail.com>
2025-11-20 09:09:17 +0900

    add type

--- main.gd
+++ main.gd
@@ -2,10 +2,10 @@ extends VBoxContainer
 
 
 func _on_start_loop_pressed() -> void:
-	var lp = int($HBoxContainer/LoopCount.text)
-	var st = Time.get_unix_time_from_system()
-	for i in lp:
+	var lp := int($HBoxContainer/LoopCount.text)
+	var st := Time.get_unix_time_from_system()
+	for i :int in lp:
 		pass
-	var dur = Time.get_unix_time_from_system() - st
-	var p = lp / dur
+	var dur := Time.get_unix_time_from_system() - st
+	var p := lp / dur
 	$LoopResult.text = "loop %d, %f sec, %f loop/sec" % [lp , dur, p ]
