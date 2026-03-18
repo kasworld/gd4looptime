abac4e56c0ab0a9ce75b7e294ca3398d987ccfd0
seukwonkang <kasworld@gmail.com>
2026-03-18 14:24:47 +0900

    add unrank permutation

--- permutation.gd
+++ permutation.gd
@@ -8,6 +8,22 @@ static func Factorial(n: int) -> int:
 		result *= i
 	return result
 
+# GDScript function to unrank a permutation
+# rank: The index of the permutation (0 to n!-1)
+static func Unrank(src_array :Array, rank: int) -> Array:
+	var work_array := src_array.duplicate()
+	var result_array := []
+	var fact := Factorial(work_array.size()-1)
+	for i in range(work_array.size() - 1, -1, -1):
+		var index := rank / fact
+		rank = rank % fact
+		result_array.append(work_array[index])
+		work_array.remove_at(index)
+		if i > 0:
+			fact /= i
+	return result_array
+
+
 # Main function to get all permutations as an array of arrays
 static func Array(array :Array) -> Array:
 	var output :Array = []
@@ -24,7 +40,6 @@ static func _array_helper(array :Array, start_index :int, output :Array) -> void
 
 	for i :int in range(start_index, array.size()):
 		# Swap current element with the element at the start index
-		#array.swap(start_index, i)
 		var tmp = array[start_index]
 		array[start_index] = array[i]
 		array[i] = tmp
@@ -33,8 +48,6 @@ static func _array_helper(array :Array, start_index :int, output :Array) -> void
 		_array_helper(array, start_index + 1, output)
 
 		# Backtrack: swap them back to restore the original array state for the next iteration
-		#array.swap(start_index, i)
-		#tmp = array[i]
 		array[i] = array[start_index]
 		array[start_index] = tmp
 
