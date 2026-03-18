2cc9ee758f048fb6102a552ee38f614c8c430df1
seukwonkang <kasworld@gmail.com>
2026-03-18 12:00:30 +0900

    split benchhelper

--- main.gd
+++ main.gd
@@ -1,52 +1,6 @@
 extends VBoxContainer
 
 
-class Timed:
-	var count :int
-	var start_time :float
-	var dur_sec :float
-	var val_per_sec :float
-	func _init(count_init ) -> void:
-		count = int(count_init)
-		start_time = Time.get_unix_time_from_system()
-	func end(new_count :int = count) -> void:
-		count = new_count
-		dur_sec = Time.get_unix_time_from_system() - start_time
-		val_per_sec = count / dur_sec
-	func get_result() -> String:
-		return "count %d, %f sec, %f count/sec" % [count , dur_sec, val_per_sec ]
-
-func _on_start_loop_pressed() -> void:
-	var timed := Timed.new($HBoxContainer/LoopCount.text)
-	for i :int in timed.count :
-		pass
-	timed.end()
-	$LoopResult.text = timed.get_result()
-
-
-func _on_start_alloc_pressed() -> void:
-	var timed := Timed.new($HBoxContainer/LoopCount.text)
-	var ar :PackedByteArray = []
-	var err := ar.resize(timed.count)
-	if err != OK:
-		$LoopResult.text = "fail resize %d" % err
-		return
-	timed.end()
-	$LoopResult.text = timed.get_result()
-
-func _on_start_permutation_pressed() -> void:
-	var timed := Timed.new(10)
-	var ar := []
-	var err := ar.resize(timed.count)
-	if err != OK:
-		$LoopResult.text = "fail resize %d" % err
-		return
-	for i :int in timed.count :
-		ar[i] = i
-	var per_ar := generate_all_permutations(ar)
-	timed.end(per_ar.size())
-	$LoopResult.text = timed.get_result()
-
 
 # Main function to get all permutations as an array of arrays
 static func generate_all_permutations(array :Array) -> Array:
@@ -77,3 +31,48 @@ static func _recursive_permutation_helper(array :Array, start_index :int, output
 		#tmp = array[i]
 		array[i] = array[start_index]
 		array[start_index] = tmp
+
+
+func _on_start_loop_pressed() -> void:
+	var timed := BenchHelper.new($HBoxContainer/LoopCount.text)
+	for i :int in timed.count :
+		pass
+	timed.end()
+	$LoopResult.text = timed.get_result()
+
+
+func _on_start_alloc_pressed() -> void:
+	var timed := BenchHelper.new($HBoxContainer/LoopCount.text)
+	var ar :PackedByteArray = []
+	var err := ar.resize(timed.count)
+	if err != OK:
+		$LoopResult.text = "fail resize %d" % err
+		return
+	timed.end()
+	$LoopResult.text = timed.get_result()
+
+func _on_start_permutation_pressed() -> void:
+	var timed := BenchHelper.new(10)
+	var ar := []
+	var err := ar.resize(timed.count)
+	if err != OK:
+		$LoopResult.text = "fail resize %d" % err
+		return
+	for i :int in timed.count :
+		ar[i] = i
+	var per_ar := generate_all_permutations(ar)
+	timed.end(per_ar.size())
+	$LoopResult.text = timed.get_result()
+
+func _on_start_permut_byte_pressed() -> void:
+	var timed := BenchHelper.new(10)
+	var ar :Array[int] = []
+	var err := ar.resize(timed.count)
+	if err != OK:
+		$LoopResult.text = "fail resize %d" % err
+		return
+	for i :int in timed.count :
+		ar[i] = i
+	var per_ar := generate_all_permutations(ar)
+	timed.end(per_ar.size())
+	$LoopResult.text = timed.get_result()
