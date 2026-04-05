14597e21fa26ced73d03f61c192bfb7ce00ca87d
seukwonkang <kasworld@gmail.com>
2026-04-05 10:08:43 +0900

    get_count

--- main.gd
+++ main.gd
@@ -1,8 +1,18 @@
 extends VBoxContainer
 
-func _on_start_loop_pressed() -> void:
+func get_count(name:String, from:int, to :int) -> int:
 	var count := int($HBoxContainer/LoopCount.text)
-	$LoopResult.text += "loop %d\n" % count
+	$LoopResult.text = "%s %d (%d ~ %d) \n" % [name, count, from, to]
+	if count < from or count > to:
+		$LoopResult.text += "out of range %d (%d ~ %d)\n" % [count, from, to]
+		return -1
+	return count
+
+
+func _on_start_loop_pressed() -> void:
+	var count := get_count("loop", 1, 1000000000000)
+	if count < 0:
+		return
 	var timed := BenchHelper.new()
 	for i :int in count :
 		pass
@@ -11,8 +21,9 @@ func _on_start_loop_pressed() -> void:
 
 
 func _on_start_alloc_pressed() -> void:
-	var count := int($HBoxContainer/LoopCount.text)
-	$LoopResult.text += "alloc %d\n" % count
+	var count := get_count("alloc", 1, 1000000000000)
+	if count < 0:
+		return
 	var timed := BenchHelper.new()
 	var ar :PackedByteArray = []
 	var err := ar.resize(count)
@@ -23,13 +34,10 @@ func _on_start_alloc_pressed() -> void:
 	$LoopResult.text += timed.to_string()  + "\n"
 
 func _on_permut_slow_recurs_pressed() -> void:
-	var timed := BenchHelper.new()
-	var count := int($HBoxContainer/LoopCount.text)
-	$LoopResult.text += "permut_slow_recurs %d\n" % count
-	if count >= 14:
-		$LoopResult.text += "too large %d (try 10 ~ 12)\n" % count
+	var count := get_count("permut_slow_recurs", 9, 14)
+	if count < 0:
 		return
-
+	var timed := BenchHelper.new()
 	var ar := []
 	var err := ar.resize(count)
 	if err != OK:
@@ -43,12 +51,10 @@ func _on_permut_slow_recurs_pressed() -> void:
 	#print_debug(per_ar)
 
 func _on_permut_slow_recurs_p_byte_pressed() -> void:
-	var timed := BenchHelper.new()
-	var count := int($HBoxContainer/LoopCount.text)
-	$LoopResult.text += "permut_slow_recurs_p_byte %d\n" % count
-	if count >= 14:
-		$LoopResult.text += "too large %d (try 10 ~ 12)\n" % count
+	var count := get_count("permut_slow_recurs_p_byte", 9, 14)
+	if count < 0:
 		return
+	var timed := BenchHelper.new()
 	var ar :PackedByteArray = []
 	var err := ar.resize(count)
 	if err != OK:
@@ -63,12 +69,10 @@ func _on_permut_slow_recurs_p_byte_pressed() -> void:
 
 
 func _on_permutate_heap_recurs_pressed() -> void:
-	var timed := BenchHelper.new()
-	var count := int($HBoxContainer/LoopCount.text)
-	$LoopResult.text += "permutate_heap_recurs %d\n" % count
-	if count >= 14:
-		$LoopResult.text += "too large %d (try 10 ~ 12)\n" % count
+	var count := get_count("permutate_heap_recurs", 9, 14)
+	if count < 0:
 		return
+	var timed := BenchHelper.new()
 	var ar := []
 	var err := ar.resize(count)
 	if err != OK:
@@ -83,12 +87,10 @@ func _on_permutate_heap_recurs_pressed() -> void:
 
 
 func _on_permutate_heap_loop_pressed() -> void:
-	var timed := BenchHelper.new()
-	var count := int($HBoxContainer/LoopCount.text)
-	$LoopResult.text += "permutate_heap_loop %d\n" % count
-	if count >= 14:
-		$LoopResult.text += "too large %d (try 10 ~ 12)\n" % count
+	var count := get_count("permutate_heap_loop", 9, 14)
+	if count < 0:
 		return
+	var timed := BenchHelper.new()
 	var ar := []
 	var err := ar.resize(count)
 	if err != OK:
@@ -103,12 +105,10 @@ func _on_permutate_heap_loop_pressed() -> void:
 
 
 func _on_permutate_heap_loop_p_byte_pressed() -> void:
-	var timed := BenchHelper.new()
-	var count := int($HBoxContainer/LoopCount.text)
-	$LoopResult.text += "permutate_heap_loop_p_byte %d\n" % count
-	if count >= 14:
-		$LoopResult.text += "too large %d (try 10 ~ 12)\n" % count
+	var count := get_count("permutate_heap_loop_p_byte", 9, 14)
+	if count < 0:
 		return
+	var timed := BenchHelper.new()
 	var ar :PackedByteArray= []
 	var err := ar.resize(count)
 	if err != OK:
